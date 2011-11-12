631e9655400937a8e0cf6f47aa59b86b4724cfac
Logan Capaldo <logancapaldo@gmail.com>
2011-11-12 14:05:33 -0500

    The shoes hello world works

--- shoes.rb
+++ shoes.rb
@@ -1,5 +1,6 @@
 require 'WindowsBase'
 require 'PresentationFramework'
+require 'PresentationCore'
 
 module Shoes
   class Instance
@@ -15,6 +16,18 @@ module Shoes
     def runloop
       System::Windows::Application.new.Run(@win)
     end
+
+    def button(content, &clickhandler)
+      but = System::Windows::Controls::Button.new
+      but.Content = content
+      @win.Content = but
+      if clickhandler
+        but.AddHandler(System::Windows::Controls::Primitives::ButtonBase::click_event, System::Windows::RoutedEventHandler.new { |o, e| clickhandler.call })
+      end
+    end
+    def alert(msg)
+      System::Windows::MessageBox.Show(@win, msg, "Shoes says")
+    end
   end
 def self.app(props, &block)
   inst = Instance.new(props)
@@ -24,5 +37,6 @@ end
 end
 
 Shoes.app :width => 300, :height => 300 do
+button("Click me!") { alert("Hello, world!") }
 end
 
